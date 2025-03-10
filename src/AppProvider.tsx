1361d127610dcc33a0f2ce1bb2b4ebee7287648c
bankai <ozcan.krakus@gmail.com>
2025-03-10 20:35:14 +0300

    fixed overflow error

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -1,14 +1,13 @@
-import React from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
-import { ToastContainer, toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export const AppProvider = () => {
   return (
-    <div>
+    <>
       <ToastContainer />
-      <RouterProvider router={router} />;
-    </div>
+      <RouterProvider router={router} />
+    </>
   );
 };
