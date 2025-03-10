5275c0ca32f7a43012d1b45527bcf807a2f4e3d0
OguzhanBek <OguzhannBeK2000@gmail.com>
2025-03-10 23:01:46 +0300

    Yerel değişiklikler kaydedildi

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -1,25 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
-<<<<<<< HEAD
-import { ToastContainer} from "react-toastify";
-=======
 import { ToastContainer } from "react-toastify";
->>>>>>> 1361d127610dcc33a0f2ce1bb2b4ebee7287648c
 import "react-toastify/dist/ReactToastify.css";
 
 export const AppProvider = () => {
   return (
-<<<<<<< HEAD
     <div className=" tutaaaaaaaaaaaaaaaa">
       <ToastContainer />
       <RouterProvider router={router} />;
       
     </div>
-=======
-    <>
-      <ToastContainer />
-      <RouterProvider router={router} />
-    </>
->>>>>>> 1361d127610dcc33a0f2ce1bb2b4ebee7287648c
   );
 };
